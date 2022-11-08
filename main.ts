90dfb46a03b7d8c995864b1aa39e36f611bfb89b
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-11-07 19:31:45 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -16,7 +16,7 @@ let variable = scene.createRenderable(zLayer1, (image1: Image, camera: scene.Cam
         for (let x = 0; x < screenClone.width; x++) {
             for (let y = 0; y < screenClone.height; y++) {
                 if (Math.percentChance(screenStatic)) {
-                    screenClone.setPixel(x, y, pixelArray[randint(0, 15)])
+                    image1.setPixel(x, y, 0)
                 }
             }
         }
