af07827f4576f2a7e311712859153b099d6e5f62
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-11-07 20:10:36 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -144,7 +144,7 @@ let variable = scene.createRenderable(zLayer1, (image1: Image, camera: scene.Cam
             }
         }
         for (let i = 0; i < 160; i++) {
-            helpers.imageBlitRow(screenClone, i, 0, staticImg, randint(1,10), 0)
+            helpers.imageBlitRow(screenClone, i, 1, staticImg, randint(1,10), 1)
         }
     }
     if (blurSize1 != 1) {
