5245f35792c4b2f4e010dcba36a51b3e5ae6cbf5
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-11-08 18:46:52 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -24,14 +24,12 @@ game.onUpdateInterval(200, function() {
 let variable = scene.createRenderable(zLayer1, (image1: Image, camera: scene.Camera) => {
     let screenClone = image1.clone()
     if (screenStatic > 0) {
-        for (let x = 0; x < 4; x++) {
-            for (let y = 0; y < 4; y++) {
-                helpers.imageBlitRow(screenClone, x, 0, staticImg, 1, 120)
-                if (Math.percentChance(50)) {
-                    staticImg.flipX()
-                } else {
-                    staticImg.flipY()
-                }
+        for (let x = 0; x < 160; x++) {
+            helpers.imageBlitRow(screenClone, x, 0, staticImg, 1, 120)
+            if (Math.percentChance(50)) {
+                staticImg.flipX()
+            } else {
+                staticImg.flipY()
             }
         }
     }
