cbc80ad4795a442c45c871e2b7b2b412628445c8
Kiwiphoenix <kiwiphoenix364@gmail.com>
2022-11-22 23:20:29 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -158,17 +158,32 @@ namespace screenEffects {
         return Buffer.create(size)
     }
     //% block="get row from image $image at y $y and put into buffer $buf"
-    //% draggableParameters="image"
     //% weight=6
     //% group=Advanced
     export function getRowsBlock(image: Image = null, buf: Buffer, y: number) {
         image.getRows(y, buf)
     }
     //% block="set row from buffer $buf to image $image at y $y"
-    //% draggableParameters="image"
     //% weight=7
     //% group=Advanced
-    export function setRowsBlock(image: Image = null, buf: Buffer, y: number) {
+    export function setRowsBlock(image: Image = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, buf: Buffer, y: number) {
         image.setRows(y, buf)
     }
 }
