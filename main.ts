a77f7e7ab60cf1ef71afd4d2caa1265db9607592
Kiwiphoenix <kiwiphoenix364@gmail.com>
2022-11-25 17:20:15 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -21,7 +21,7 @@ let variable = scene.createRenderable(zLayer1, (image1: Image, camera: scene.Cam
             for (let y = 0; y < repeat; ++y) {
                 buf[randint(0, 119)] = pixelArray[randint(0, 14)]
             }
-            image1.setRows(x, buf)
+            image1.setRows(x * zoomSize1 + x1, buf)
         }
     }
     /*
